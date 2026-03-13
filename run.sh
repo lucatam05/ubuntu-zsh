3acc23fce949965a41a39088bf3f732bf45b7d60
Luca <tamiazzoluca2912@gmail.com>
2026-03-13 18:29:05 +0100

    Simplify container startup using Docker Compose
    Replaced Docker commands with Docker Compose command.

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # run.sh - avvia il container Ubuntu-Zsh
 
-# Pull the latest public image from GHCR (if not already downloaded)
-docker pull ghcr.io/<your-username>/ubuntu-zsh:latest
-
-# Run the container with Zsh
-docker run -it ghcr.io/<your-username>/ubuntu-zsh:latest zsh
+docker compose up
